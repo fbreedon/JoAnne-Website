db809e591faa0af251c6108ee18aa4f9a66b2030
Franz Breedon <agentlego@gmail.com>
2020-05-04 18:58:01 -0700

    Site Setup
    Initial site setup. Created an index file for the homepage and set up the basic meta information, navigation, slideshow, and footer. Created a CSS stylesheet and set up a table of contents and initial classes. Started work on the homepage slideshow  by creating a script with image animations.

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+  	<meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+  	<meta name="description" content="">
+    <meta name="keywords" content="">
+		<link rel="icon" type="image/png" sizes="32x32" href="">
+		<link rel="manifest" href="./site.webmanifest">
+  	<meta property="og:image:url" content="">
+  	<meta property="og:image:width" content="">
+  	<meta property="og:image:height" content="">
+  	<meta property="og:type" content="website">
+  	<meta property="og:url" content="">
+  	<meta property="og:title" content="">
+  	<meta property="og:description" content="">
+  	<title>Home | Joanne Fillatti</title>
+  </head>
+
+  <body>
+
+    <!-- Homepage header -->
+    <header class="header">
+      <!-- Header title -->
+      <a href="./" class="title">JoAnne Fillatti Photography</a>
+      <!-- Header navigation container -->
+      <nav class="navigation" id="navigation-id">
+        <!-- Navigation links -->
+        <a href="./birds/">Birds</a>
+        <a href="./wildlife/">Wildlife</a>
+        <a href="./landscapes/">Landscapes</a>
+        <a href="./abstracts/">Abstracts</a>
+        <a href="./other/">Other</a>
+      </nav>
+    </header>
+
+    <!-- Content container for centering -->
+    <div class="content">
+      <!-- Slideshow container -->
+      <div class="slideshow">
+        <!-- Slideshow images -->
+        <!-- Php to read the image files and display them in the slideshow -->
+          <?php
+          // Find all the image file paths in the directory and store them in an array,
+          // then display each one properly in the slideshow
+          foreach (glob("images/slideshow/*.{JPG,jpg,gif,png,bmp}", GLOB_BRACE) as $img_file) {
+          ?>
+            <img class="slide-img" src="<?php echo $img_file; ?>" />
+          <?php
+          }
+          ?>
+      </div>
+    </div>
+
+    <!-- Call the slideshow script -->
+    <script type="./scripts/slideshow script.js"></script>
+
+    <!-- Call the mobile navigation icon script -->
+
+    <!-- Footer for copyright and social media info -->
+    <footer class="footer">
+      <div class="footer-left">
+        <p>&copy JoAnne Fillatti</p>
+      </div>
+    </footer>
+
+  </body>
+
+</html>
