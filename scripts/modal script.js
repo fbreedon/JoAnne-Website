3b4885a909505381437051a85d2e285e54457fee
Franz Breedon <agentlego@gmail.com>
2020-06-12 23:10:59 -0700

    Modal Bugfixing
    Fixed some issues with the modal rework, particularly with browsers other than Chrome. Now calculates the image container height when the modal is opened, instead of when switching images, as Firefox and Edge weren't displaying the calculated height the first time the image was viewed. Now sets the image capacity to 0 when closing, so it doesn't transition from the previous image when opening the modal again. Recalculates the next image's height when switching to get Firefox and Edge to display the actual resized height instead of the image's base height. Added -webkit- selectors to work with more browsers. Added will-change on hover to make transitions smoother. Commented new script code. Fixed an issue where the first image opened would transition from the first image in the gallery, since the image index is set to 1. Issue with Edge where the first time the modal is opened, the images shift when the browser calculates the new height (but no one uses Edge so it's not a big deal).

--- scripts/modal script.js
+++ scripts/modal script.js
@@ -1,15 +1,18 @@
 // Function to open the modal when clicking an image
 // Saves the scroll value of the page and makes it so you can't scroll
+// Calls a function to set the image container height
 const openModal = () => {
 	document.getElementById('modal-id').style.display = "block";
 	const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
 	const body = document.body;
 	body.style.position = 'fixed';
 	body.style.top = `-${scrollY}`;
+	calcHeight();
 };
 
 // Function to close the modal when clicking the close button
 // Scrolls the page back to the original scroll value when the modal opened
+// Sets image opacity to 0 so it doesn't transition when reopening the modal
 const closeModal = () => {
 	const body = document.body;
   const scrollY = body.style.top;
@@ -17,11 +20,18 @@ const closeModal = () => {
   body.style.top = '';
   window.scrollTo(0, parseInt(scrollY || '0') * -1);
 	document.getElementById('modal-id').style.display = "none";
+	for (i = 0; i < images.length; i++) {
+		images[i].style.opacity = "0";
+	}
 };
 
-// Set the index for the images and call the show function
-var modalIndex = 1;
-showModal(modalIndex);
+// Get the html elements by their class name
+var images = document.getElementsByClassName("modal-img");
+var captionText = document.getElementsByClassName("caption");
+var imageContainer = document.getElementsByClassName("modal-container");
+
+// Set a variable for the image index
+var modalIndex;
 
 // Take in an integer and increase the image index that many times
 // Used to scroll through the images
@@ -39,11 +49,6 @@ function currentModal(n) {
 // index, clear the images and info, then set the correct image and info to show
 function showModal(n) {
 	var i;
-	var images = document.getElementsByClassName("modal-img");
-	var captionText = document.getElementsByClassName("caption");
-	//var imageContainer = document.getElementsByClassName("test");
-
-	calcHeight();
 
 	// Loop back to the first or last image when at either end of the modal
 	if (n > images.length) {modalIndex = 1;}
@@ -51,7 +56,6 @@ function showModal(n) {
 
 	// Set the opacity for each image to 0
 	for (i = 0; i < images.length; i++) {
-		//images[i].style.display = "none";
 		images[i].style.opacity = "0";
 	}
 
@@ -62,20 +66,17 @@ function showModal(n) {
 		}
 	}
 	
-	//imageContainer[modalIndex-1].style.width = parseInt(window.getComputedStyle(images[modalIndex-1]).width) + "px";
-	//imageContainer[modalIndex-1].style.height = parseInt(window.getComputedStyle(images[modalIndex-1]).height) + "px";
+	// Calculate the image's resized height and set the image container's height to it
+	imageContainer[modalIndex-1].style.height = parseInt(window.getComputedStyle(images[modalIndex-1]).height) + "px";
 	// Set the current image's opacity to 1
-	//images[modalIndex-1].style.display = "block";
 	images[modalIndex-1].style.opacity = 1;
 	// Check that there are captions, and display the current image's caption
 	if(captionText.length > 0) {captionText[modalIndex-1].style.display = "block";}
 }
 
+// Function to set the image container height based on the image's size after responsive resizing
 function calcHeight() {
 	var i;
-	var images = document.getElementsByClassName("modal-img");
-	var imageContainer = document.getElementsByClassName("modal-container");
-
 	for (i = 0; i < images.length; i++) {
 		imageContainer[i].style.height = parseInt(window.getComputedStyle(images[i]).height) + "px";
 	}
